c398f44e658a01e78133aa335c43146d598b3317
Kostya Mileant <kostya.milya@gmail.com>
2025-08-15 14:59:05 +0200

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(blank=True)
+    description = models.CharField(max_length=255, blank=False)
     duration = models.IntegerField()
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,18 +1,9 @@
 from rest_framework import serializers
 from .models import Movie
 
-class MovieSerializers(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'duration', 'description']
+        fields = ["id", "title", "duration", "description"]
 
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.duration = validated_data.get('duration', instance.duration)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
-        return instance
